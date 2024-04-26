e49a1c7ddfc7df6256b53e9686cab72bebba4914
Yoav TC <yoavtc2004@gmail.com>
2024-04-26 08:27:09 +0300

    Started redoing the audio manager
    im so tired. on 0h of sleep lol

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -9,103 +9,31 @@ using Random = UnityEngine.Random;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    [SerializeField] private float fadeDuration = 0.25f;
-    
-    
-    public void PlaySound(AudioClip audioClip, SoundOptions soundOptions)
+    [SerializeField] private AudioSource sfxObject;
+    public void PlaySFX(AudioClip audioClip)
     {
-        AudioSource audioSource = GetAudioSource(audioClip);
-        audioSource.volume = soundOptions.volume;
-        audioSource.loop = soundOptions.loop;
-        if (soundOptions.randomPitch)
-        {
-            audioSource.pitch = Random.Range(soundOptions.pitchMinMax.x, soundOptions.pitchMinMax.y);
-        } else audioSource.pitch = soundOptions.pitch;
-        
-        audioSource.PlayDelayed(soundOptions.delay);
+        AudioSource audioSource = Instantiate(sfxObject);
+        audioSource.clip = audioClip;
+        audioSource.Play();
+
+        float clipLength = audioClip.length;
+        Destroy(audioSource.gameObject, clipLength);
     }
 
-    public void PauseSound(AudioClip audioClip, bool fade, Action onPauseComplete)
+    public void PlaySFX(AudioClip audioClip, AudioSource audioSource)
     {
-        AudioSource audioSource = GetAudioSource(audioClip);
-        if (fade)
-        {
-            audioSource.DOFade(0f, fadeDuration).OnComplete(() =>
-            {
-                audioSource.Pause();
-                onPauseComplete?.Invoke();
-            });
-        } else {
-            audioSource.Pause();
-        }
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
     
-    public void ResumeSound(AudioClip audioClip, bool fade, float volume = 1f)
+    public void PlaySFX(AudioClip[] audioClips)
     {
-        AudioSource audioSource = GetAudioSource(audioClip);
-        audioSource.UnPause();
-        audioSource.volume = 0;
-        
-        if (fade)
-        {
-            audioSource.DOFade(volume, fadeDuration);
-        } else {
-            audioSource.volume = volume;
-        }
+        AudioSource audioSource = Instantiate(sfxObject);
+        int randomClipIndex = Random.Range(0, audioClips.Length);
+        audioSource.clip = audioClips[randomClipIndex];
+        audioSource.Play();
+
+        float clipLength = audioClips[randomClipIndex].length;
+        Destroy(audioSource.gameObject, clipLength);
     }
-
-    public void StopSound(AudioClip audioClip, bool fade)
-    {
-        AudioSource audioSource = GetAudioSource(audioClip);
-        PauseSound(null, fade, () =>
-        {
-            Destroy(audioSource);
-        });
-    }
-
-    private AudioSource GetAudioSource(AudioClip audioClip)
-    {
-        AudioSource[] audioSources = gameObject.GetComponents<AudioSource>();
-        List<AudioSource> deleteLater = new List<AudioSource>();
-        
-        for (int i = 0; i < audioSources.Length; i++)
-        {
-            if (!audioSources[i].isPlaying && audioSources[i].volume > 0f)
-            {
-                deleteLater.Add(audioSources[i]);
-            }
-            
-            if (audioSources[i].clip == audioClip)
-            {
-                return audioSources[i];
-            }
-        }
-
-        foreach (var audioSource in deleteLater)
-        {
-            Destroy(audioSource);
-        }
-        
-        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
-        newAudioSource.clip = audioClip;
-        return newAudioSource;
-    }
-}
-
-[Serializable]
-public class SoundOptions
-{
-    public SoundOptions(float volume, float pitch, float delay, bool loop, bool randomPitch, Vector2 pitchMinMax)
-    {
-        this.volume = volume;
-        this.pitch = pitch;
-        this.delay = delay;
-        this.pitchMinMax = pitchMinMax;
-        this.loop = loop;
-        this.randomPitch = randomPitch;
-    }
-
-    public float volume, pitch, delay;
-    public bool loop, randomPitch;
-    public Vector2 pitchMinMax;
 }
